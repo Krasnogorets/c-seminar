790262bda23e9864aeb26b68434ac47cfc505b63
Philipp_Kurpekov <p.kurpekov@gmail.com>
2023-01-05 17:58:31 +0300

    add random its not works

--- seminar1/Exam002/Program.cs
+++ seminar1/Exam002/Program.cs
@@ -1,3 +1,14 @@
 ﻿/*int numberA = 3;
 int numberB = 5;
 Console.WriteLine(numberA + numberB);*/
+/*double numberA = 12;
+double numberB = 0.3;
+Console.WriteLine (numberA);
+Console.WriteLine (numberB);
+Console.WriteLine (numberA / numberB);*/
+
+int numberC = new random().Next(1, 100);
+int numberD = new random().Next(1, 100);
+Console.WriteLine (numberA);
+Console.WriteLine (numberB);
+Console.WriteLine (numberA / numberB);
