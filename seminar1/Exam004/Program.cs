bcabe6ce2f784575e99d7f0cc5c88b13c66d4cf3
Philipp_Kurpekov <p.kurpekov@gmail.com>
2023-01-06 11:21:44 +0300

    exam004 added

--- seminar1/Exam004/Program.cs
+++ seminar1/Exam004/Program.cs
@@ -1,1 +1,41 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+/*Console.SetCursorPosition(10,10);
+Console.WriteLine("+");*/
+int xa = 40;
+int ya = 2;
+int xb = 2;
+int yb = 20;
+int xc = 80;
+int yc = 20;
+int x = 0;
+int y = 0;
+Console.SetCursorPosition(xa,ya);
+Console.WriteLine("+");
+Console.SetCursorPosition(xb,yb);
+Console.WriteLine("+");
+Console.SetCursorPosition(xc,yc);
+Console.WriteLine("+");
+int count = 0;
+while (count < 5000)
+{
+    int rand = new Random().Next(0, 3);
+    if (rand == 0)
+    {
+        x = (x+xa)/2;
+        y = (y+ya)/2; 
+    }
+    if (rand == 1)
+    {
+        x = (x+xb)/2;
+        y = (y+yb)/2; 
+    }
+    if (rand == 2)
+    {
+        x = (x+xc)/2;
+        y = (y+yc)/2; 
+    }
+Console.SetCursorPosition(x,y);
+Console.WriteLine("+");
+count++;
+
+}
