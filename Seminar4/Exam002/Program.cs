d59ed45a16ffd04ad206a5258f9d775ab8e8d407
Philipp_Kurpekov <p.kurpekov@gmail.com>
2023-01-12 21:14:56 +0300

    seminar4 finished

---
+++ Seminar4/Exam002/Program.cs
@@ -0,0 +1,66 @@
+﻿Console.Clear();
+/*int Max (int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int [] massive = {1,2,3345,456,5,68,7,8,9,10};
+massive[1] = 253;
+
+int max = Max ( Max(massive[0],massive[1],massive[2]),Max(massive[3],massive[4],massive[5]),Max(massive[6],massive[7],massive[8]));
+Console.WriteLine (max);*/
+/*int [] massive = {1,2,5,456,5,68,7,8,9,10};
+int n = massive.Length;
+int find = 5;
+for (int index = 0; index < n; index++)
+{
+    if (massive[index] == find) 
+    {
+        Console.Write($"{index} ");
+        break;
+    }
+    
+}*/
+
+
+void FillArray(int[] collection) /*FillArray - название метода*/
+{
+    int len = collection.Length;
+    for (int i = 0; i < len; i++)
+    {
+        collection[i] = new Random().Next(1, 10);
+    }
+}
+void PrintArray(int [] collection1)
+{
+    int count = collection1.Length;
+    for (int i2 = 0; i2 < count; i2++)
+    {
+        Console.Write($"{collection1[i2] } ");
+    }
+}
+int IndexOf(int[] collection2, int find)
+{
+    int count = collection2.Length;
+    int position = -1;
+    for (int i3 = 0; i3< count; i3++)
+    {
+        if (collection2[i3] == find)
+        {
+            position = i3;
+            break;
+        }
+    }
+    return position;
+}
+int [] massive = new int[10];
+FillArray(massive);
+massive[3] = 4;
+massive[6] = 4;
+PrintArray(massive);
+Console.WriteLine();
+int pos = IndexOf(massive, 444);
+Console.WriteLine(pos);
---
+++ Seminar4/Exam003/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+456 -> 3
+
+78 -> 2
+
+89126 -> 5*/
+/* метод*/
+/*int GetMultiply (int arg1)
+{
+    int res = 1;
+   for (int i = 1; i < arg1; i++)
+   {
+    res = res * i;
+   }
+    return res;
+}
+
+Console.Clear();
+Console.WriteLine("Введите число:");
+int arg1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine ($"Произведение чисел от 1 до{arg1} равно {GetMultiply(arg1)}");*/
+
+int[] GetArray(int size){
+    int[] myArray = new int[size];
+    for(int i = 0; i < size; i++){
+        myArray[i] = new Random().Next(2);
+    }
+    return myArray;
+}
+
+int[] myArray = GetArray(8);
+Console.WriteLine($"[{String.Join(",", myArray)}]");
