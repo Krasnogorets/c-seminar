1afc19791d75f78722f966694fe7e6ead75eb381
Philipp_Kurpekov <p.kurpekov@gmail.com>
2023-01-16 22:10:19 +0300

    seminar 11 added

--- HomeWork6/Program.cs
+++ HomeWork6/Program.cs
@@ -3,15 +3,17 @@
 // 1, -7, 567, 89, 223-> 3
 Console.Clear();
 Console.WriteLine("Введите чиcла через пробел");
-Console.WriteLine($"количество положительных = {GetCount(Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries))}");
+Console.WriteLine(
+    $"количество положительных = {GetCount(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries))}"
+);
 
 //method
-int GetCount (string[] l)
+int GetCount(string[] l)
 {
     int count = 0;
     for (int i = 0; i < l.Length; i++)
     {
-        if (Convert.ToInt32(l[i])>0)
+        if (Convert.ToInt32(l[i]) > 0)
         {
             count++;
         }
@@ -19,26 +21,26 @@ int GetCount (string[] l)
     return count;
 }
 
-// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
 // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
 
 Console.WriteLine("Введите чиcла b1, k1, b2 и k2 через пробел:");
-string[] numbers = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries);
+string[] numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
 Console.WriteLine($" точка пересечения прямых = ( {String.Join(", ", GetPoint(numbers))} )");
 
 //метод
-double [] GetPoint (string[] nums)
+double[] GetPoint(string[] nums)
 {
-    double[] temp = new double [nums.Length];
-    double[] result = new double [2];
+    double[] temp = new double[nums.Length];
+    double[] result = new double[2];
     for (int i = 0; i < temp.Length; i++)
     {
-        temp[i] = Convert.ToInt32(nums[i]); 
+        temp[i] = Convert.ToInt32(nums[i]);
     }
     // х = (b2-b1)/(k1-k2)
     // y = k1 * x + b1
-    result[0] = (temp[0]-temp[2])/(temp[3]-temp[1]);
+    result[0] = (temp[0] - temp[2]) / (temp[3] - temp[1]);
     result[1] = temp[1] * result[0] + temp[0];
     return result;
 }
---
+++ Seminar11/Program.cs
@@ -0,0 +1,131 @@
+﻿// массив 3,4 заполнить случ числами
+/*Console.Clear();
+Console.Write("Введите размер массива через пробел m*n:");
+string[] param = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+PrintArray(CreateArray(int.Parse(param[0]), int.Parse(param[1]), -10, 100));
+//метод
+int[,] CreateArray(int rows, int col, int min, int max)
+{
+    int[,] result = new int[rows, col];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] InArray)
+{
+    for (int i = 0; i < InArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < InArray.GetLength(1); j++)
+        {
+            Console.Write($"{InArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+*/
+// заполнить массив где Amn = m+n
+/*Console.Clear();
+Console.Write("Введите размер массива через пробел m*n:");
+string[] param = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+PrintArray(CreateArray(int.Parse(param[0]), int.Parse(param[1])));
+//метод
+int[,] CreateArray(int rows, int col)
+{
+    int[,] result = new int[rows, col];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = i + j;
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] InArray)
+{
+    for (int i = 0; i < InArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < InArray.GetLength(1); j++)
+        {
+            Console.Write($"{InArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+*/
+// найти четные индексы, заменить содержимое на квадрат
+Console.Clear();
+Console.Write("Введите размер массива через пробел m*n:");
+string[] param = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int[,] array = CreateArray(int.Parse(param[0]), int.Parse(param[1]));
+PrintArray(array);
+Console.WriteLine();
+Console.Write($"сумма по главной диагонали:{SumArray(array)}");
+Console.WriteLine();
+Console.Write($"сумма по главной диагонали:{GetSum(array)}");
+//ChangeArray(array);
+
+//PrintArray(array);
+//метод
+int[,] CreateArray(int rows, int col)
+{
+    int[,] result = new int[rows, col];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = i + j;
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] InArray)
+{
+    for (int i = 0; i < InArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < InArray.GetLength(1); j++)
+        {
+            Console.Write($" {InArray[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
+void ChangeArray(int[,] InArray)
+{
+    for (int i = 2; i < InArray.GetLength(0); i += 2)
+    {
+        for (int j = 2; j < InArray.GetLength(1); j += 2)
+        {
+            InArray[i, j] *= InArray[i, j];
+        }
+    }
+}
+int SumArray(int[,] InArray)
+{
+    int sum = 0;
+    for (int i = 0; i < InArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < InArray.GetLength(1); j++)
+        {
+
+            if (i == j)sum += InArray[i, j];
+        }
+    }
+    return sum;
+}
+int GetSum (int[,]inArray)
+{
+    int sum = 0;
+    int length = inArray.GetLength(0);
+    if (length>inArray.GetLength(0))length = inArray.GetLength(1);
+    for (int i = 0; i < length; i++)
+    {
+        sum += inArray[i, i];
+    }
+     return sum;
+}
