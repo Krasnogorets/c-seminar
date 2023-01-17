90ea25d36daea8a9fb8b23e38edd640fa3978b52
Philipp_Kurpekov <p.kurpekov@gmail.com>
2023-01-17 18:18:06 +0300

    Seminar 13 example added doesnt work

---
+++ Seminar13/Program.cs
@@ -0,0 +1,16 @@
+﻿// обработка введенного текста, вычленение нужных данных - не работает
+using System.Linq; 
+string text  ="(1,2) (2,3) (4,5) (6,7)"
+    .Replace("(","")
+    .Replace(")","");
+Console.WriteLine(text);
+var data = text.Split(" ")
+                .Select(item => item.Split(","))
+                .Select(e=> x: item.Parse(e[0]),  y: item.Parse(e[1]))
+                .ToArray();
+for (int i = 0; i < data.Length; i++)
+{
+    Console.WriteLine(data[i].x*10);
+}
+
+
