87df7bc79eb3b271be85bca14075e7a1b2f1ab94
Philipp_Kurpekov <p.kurpekov@gmail.com>
2023-01-18 21:34:06 +0300

    HW9 task3 done

--- HomeWork9/Program.cs
+++ HomeWork9/Program.cs
@@ -25,7 +25,7 @@ string PrintNumbers(int start, int finish)
 /*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30*/
-Console.Clear();
+/*Console.Clear();
 Console.Write("введите значение M:");
 int M = int.Parse(Console.ReadLine()!);
 Console.Write("введите значение N:");
@@ -40,8 +40,34 @@ else
 }
 
 //рекурсивный метод
-int SumGap(int start, int finish) 
+int SumGap(int start, int finish)
 {
-    if (start > finish) return 0;// обозначить условие выхода из нее 
+    if (start > finish) return 0;// обозначить условие выхода из нее
     return (start + SumGap(start + 1, finish));
+}
+*/
+/*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 29*/
+Console.Clear();
+Console.Write("введите значение M:");
+int M = int.Parse(Console.ReadLine()!);
+Console.Write("введите значение N:");
+int N = int.Parse(Console.ReadLine()!);
+if ( (M < 0) || (N < 0) || (M > 4) || (N > 4))
+{
+    Console.Write($"Введенные значения не соответсвуют условию");
+}
+else
+{
+    Console.Write($"Функция Аккермана A({M}, {N}) ->{Akkerman(M, N)}");
+}
+int Akkerman(int M, int N)
+{
+    if (M == 0) return N + 1;
+    else
+    {
+        if (N == 0) return Akkerman(M - 1, 1);
+    }
+    return Akkerman(M - 1, Akkerman(M, N - 1));
 }
