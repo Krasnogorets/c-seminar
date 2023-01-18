5ff9dc201f35aad911404f00672fc33c32d44076
Philipp_Kurpekov <p.kurpekov@gmail.com>
2023-01-18 20:42:15 +0300

    HW9 task1 done

---
+++ HomeWork9/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+M = 4; N = 8. -> ""4, 6, 7, 8""*/
+Console.Clear();
+Console.Write("введите значение M:");
+int M = int.Parse(Console.ReadLine()!);
+Console.Write("введите значение N:");
+int N = int.Parse(Console.ReadLine()!);
+if (M>N)
+{
+    Console.Write($"{M} > {N}  что не соответсвует условию");
+}
+else
+{
+    Console.Write($"M = {M}; N = {N}. ->{PrintNumbers(M,N)}");
+}
+//рекурсивный метод
+string PrintNumbers(int start, int finish)
+{
+    if (start == finish)
+        return start.ToString();
+    return (start + " " + PrintNumbers(start + 1, finish));
+}
---
+++ Seminar15/Program.cs
@@ -0,0 +1,41 @@
+﻿// примеры на рекурсию
+/*Console.Clear();
+
+// вывести все натуральные числа от 1 до N
+Console.Write("введите значение N:");
+int N = int.Parse(Console.ReadLine()!);
+Console.Write(PrintNumbers(1, N));
+
+string PrintNumbers(int start, int end)
+{
+    if (start == end)
+        return start.ToString();
+    return (start + " " + PrintNumbers(start + 1, end));
+}
+*/
+// подсчитать сумму цифр числа через рекурсию
+/*Console.Write("введите число");
+int number = int.Parse(Console.ReadLine()!);
+Console.Write($"Сумма цифр = {Sum(number)}");
+
+int Sum(int num) // основное правило рекурсии обозначить условие выхода из нее
+{
+    if (num == 0) return 0;
+    return (number % 10 + Sum(number / 10));
+}
+*/
+// возмедение а в степень б
+Console.Write("введите число A : ");
+int numberA = int.Parse(Console.ReadLine()!);
+Console.Write("введите число B : ");
+int numberB = int.Parse(Console.ReadLine()!);
+
+Console.Write($"{numberA} в степени {numberB} = {Pow(numberA,numberB)}");
+
+int Pow (int num, int st)
+{
+    if(st == 0) return 1;
+    if(st== 1) return num;
+    return(num*Pow(num,st-1));
+
+}
