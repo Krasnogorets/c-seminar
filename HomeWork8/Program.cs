af1f5c5ee2284dff265bb2029c2a87b33c1ea23e
Philipp_Kurpekov <p.kurpekov@gmail.com>
2023-01-18 12:57:31 +0300

    HW8 task2 done

--- HomeWork8/Program.cs
+++ HomeWork8/Program.cs
@@ -17,13 +17,14 @@
 
 2 4 4 8
 */
-Console.Clear();
-int[,] NewArray = GetArray (4,12,1,10);
+/*Console.Clear();
+int[,] NewArray = GetArray(4, 12, 1, 10);
 PrintArray(NewArray);
 Console.WriteLine();
 NewArrayOrder(NewArray);
 PrintArray(NewArray);
 Console.WriteLine();
+
 //method
 int[,] GetArray(int rows, int col, int min, int max)
 {
@@ -49,23 +50,109 @@ void PrintArray(int[,] inArray)
     }
 }
 
-void NewArrayOrder (int[,]inArray)
+void NewArrayOrder(int[,] inArray)
 {
     int temp = 0; // промежуточная для записи переносимых данных при сортировке
     for (int i = 0; i < inArray.GetLength(0); i++)
     {
         for (int j = 0; j < inArray.GetLength(1); j++)
         {
-            for (int k = j+1; k < inArray.GetLength(1); k++)
+            for (int k = j + 1; k < inArray.GetLength(1); k++)
             {
-                if (inArray[i,k] < inArray[i,j])
+                if (inArray[i, k] < inArray[i, j])
                 {
-                    temp = inArray[i,j];
-                    inArray[i,j] = inArray[i,k];
-                    inArray[i,k] = temp;
+                    temp = inArray[i, j];
+                    inArray[i, j] = inArray[i, k];
+                    inArray[i, k] = temp;
                 }
             }
         }
-
     }
+}
+*/
+/*Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая 
+будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+*/
+Console.Clear();
+Console.Write("Введите кол-во строк:");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Введите кол-во столбцов:");
+int col = int.Parse(Console.ReadLine()!);
+if (row == col)
+{
+    Console.Write("массив не прямоугольный");
+}
+else
+{
+    int[,] NewArray = GetArray(row, col, 1, 10);
+    PrintArray(NewArray);
+    Console.WriteLine();
+    int[,] SumArray = SumRowElements(NewArray);
+    Console.Write($"Строка с минимальной суммой элементов => {FindIndexMinRowSum(SumArray)}");
+}
+
+//method
+int[,] GetArray(int rows, int col, int min, int max)
+{
+    int[,] result = new int[rows, col];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = new Random().Next(min, max);
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($" {inArray[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] SumRowElements (int[,]inArray)
+{
+    int[,] result = new int[2,inArray.GetLength(0)];
+    
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j <  inArray.GetLength(1); j++)
+        {
+           sum+=inArray[i,j];
+        }
+        result[0,i] =  sum;
+        result[1,i] = i;
+    }
+    return result;
+}
+int FindIndexMinRowSum (int[,]inArray) // возвращает индекс строки с минимальной суммой
+{
+    int min = inArray[0,0];
+    int temp = 0;
+    int indexMin = 0;
+    for (int i = 0; i < inArray.GetLength(1); i++)
+    {
+        if (inArray[0,i] <min)
+        {
+            temp = min;
+            min = inArray[0,i];
+            indexMin = i;
+            inArray[0,i] = temp;
+        }
+    }
+    return indexMin +1; /* сделал так, чтобы возвращал не реальный индекс, 
+    а как строка визуально выглядит на экране, если сумма строк совпадает выводится первая,
+    согласно приницпу KISS, не стал усложнять, чтобы сделать доп условие, т.к. такой задачи не стояло*/
 }
