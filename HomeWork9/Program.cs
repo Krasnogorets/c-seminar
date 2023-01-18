4f7a8f6f730c4f3eccc9d0070845e68e0bb810e9
Philipp_Kurpekov <p.kurpekov@gmail.com>
2023-01-18 21:02:11 +0300

    HW9 task2 done

--- HomeWork9/Program.cs
+++ HomeWork9/Program.cs
@@ -1,7 +1,7 @@
 ﻿/*Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
 M = 1; N = 5. -> ""1, 2, 3, 4, 5""
 M = 4; N = 8. -> ""4, 6, 7, 8""*/
-Console.Clear();
+/*Console.Clear();
 Console.Write("введите значение M:");
 int M = int.Parse(Console.ReadLine()!);
 Console.Write("введите значение N:");
@@ -20,4 +20,28 @@ string PrintNumbers(int start, int finish)
     if (start == finish)
         return start.ToString();
     return (start + " " + PrintNumbers(start + 1, finish));
+}
+*/
+/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+Console.Clear();
+Console.Write("введите значение M:");
+int M = int.Parse(Console.ReadLine()!);
+Console.Write("введите значение N:");
+int N = int.Parse(Console.ReadLine()!);
+if (M >= N)
+{
+    Console.Write($"{M} >= {N}  что не соответсвует условию");
+}
+else
+{
+    Console.Write($"Сумма чисел от {M} до {N} ->{SumGap(M, N)}");
+}
+
+//рекурсивный метод
+int SumGap(int start, int finish) 
+{
+    if (start > finish) return 0;// обозначить условие выхода из нее 
+    return (start + SumGap(start + 1, finish));
 }
